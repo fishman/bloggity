9aef745c8d66fa89ad7d9750f8ef450191179eee
Reza Jelveh <toyb0mb@gmx.net>
2009-03-01 23:44:01 +0100

    fix dependencies problem in init.rb

--- init.rb
+++ init.rb
@@ -4,6 +4,6 @@ ActionController::Base.send :include, BloggityApplication
 # FIX for engines model reloading issue in development mode
 if ENV['RAILS_ENV'] != 'production'
 	load_paths.each do |path|
-		Dependencies.load_once_paths.delete(path)
+		ActiveSupport::Dependencies.load_once_paths.delete(path)
 	end
 end
